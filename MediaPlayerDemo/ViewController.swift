95d92c08a7fe053c8ba32d57c9b58b705679e381
shafujiu <zhaowei200711@163.com>
2021-07-12 14:52:33 +0800

    Initial Commit

---
+++ MediaPlayerDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MediaPlayerDemo
+//
+//  Created by Shafujiu on 2021/7/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
